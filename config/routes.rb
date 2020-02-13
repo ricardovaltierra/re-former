81b52878cb352d72e89c29d8667dce33a55800b0
ClintonEnyinna <enyinnaclinton@gmail.com>
2020-02-13 09:51:28 -0600

    fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: %i[new create edit update]
 end
